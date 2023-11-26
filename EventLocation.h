7ee1fbf4cb7f7200b69111c6f6a1627b621f29a9
mohebbisoheyl <74722171+mohebbisoheyl@users.noreply.github.com>
2023-11-26 20:16:08 +0200

    Update EventLocation.h

--- EventLocation.h
+++ EventLocation.h
@@ -1,39 +1,192 @@
-#ifndef EVENT_LOCATION_H
-#define EVENT_LOCATION_H
+#include "eventLocation.h"
+#include <string.h>
 
-#include <iostream>
+using namespace std;
+//commit update zones by making zones string* 
+EventLocation::EventLocation() : maxSeats(0), numRows(0), numZones(0), zones(nullptr), seatsPerRow(nullptr) {}
 
-class EventLocation {
-public:
-    EventLocation();
-    EventLocation(int maxSeats, int numRows, const char* zones, int seatsPerRow);
-    ~EventLocation();
-    EventLocation(const EventLocation& other);
-    EventLocation& operator=(const EventLocation& other);
+EventLocation::EventLocation(int maxSeats, int numRows, int numZones, const std::string* zones, int* seatsPerRow) {
+    this->maxSeats = maxSeats;
 
-    friend std::istream& operator>>(std::istream& in, EventLocation& eventLocation);
-    friend std::ostream& operator<<(std::ostream& out, const EventLocation& eventLocation);
+    if (numRows > 0 && seatsPerRow != nullptr) {
+        this->numRows = numRows;
+        this->seatsPerRow = new int[numRows];
+        for (int i = 0; i < numRows; i++) {
+            this->seatsPerRow[i] = seatsPerRow[i];
+        }
+    }
+    else {
+        this->numRows = 0;
+        this->seatsPerRow = nullptr;
+    }
 
-    // Getteri ?i setteri
-    int getMaxSeats() const;
-    void setMaxSeats(int maxSeats);
+    if (numZones > 0 && zones != nullptr) {
+        this->numZones = numZones;
+        this->zones = new std::string[numZones];
+        for (int i = 0; i < numZones; i++) {
+            this->zones[i] = zones[i];
+        }
+    }
+    else {
+        this->numZones = 0;
+        this->zones = nullptr;
+    }
+}
 
-    int getNumRows() const;
-    void setNumRows(int numRows);
 
-    const char* getZones() const;
-    void setZones(const char* zones);
+EventLocation::~EventLocation() {
+    delete[] zones;
+    delete[] seatsPerRow;
+}
 
-    int getSeatsPerRow() const;
-    void setSeatsPerRow(int seatsPerRow);
+EventLocation::EventLocation(const EventLocation& l){
+    this->maxSeats = l.maxSeats;
 
-private:
-    int maxSeats;
-    int numRows;
-    char* zones;
-    int seatsPerRow;
+    if (l.numRows > 0 && l.seatsPerRow != nullptr) {
+        this->numRows = l.numRows;
+        this->seatsPerRow = new int[l.numRows];
+        for (int i = 0; i < l.numRows; i++) {
+            this->seatsPerRow[i] = l.seatsPerRow[i];
+        }
+    }
+    else {
+        this->numRows = 0;
+        this->seatsPerRow = nullptr;
+    }
+    if (l.numZones > 0 && l.zones != nullptr) {
+        this->numZones = l.numZones;
+        this->zones = new string[l.numZones];
+        for (int i = 0; i < l.numZones; i++) {
+            this->zones[i] = l.zones[i];
+        }
+    }
+    else {
+        this->numZones = 0;
+        this->zones = nullptr;
+    }
 
-    void releaseMemory();
-};
+   
+}
 
-#endif // EVENT_LOCATION_H
+EventLocation& EventLocation::operator=(const EventLocation& l) {
+    if (this != &l) {
+        delete[] zones;
+        delete[] seatsPerRow;
+
+        maxSeats = l.maxSeats;
+        numRows = l.numRows;
+        this->seatsPerRow = new int[l.numRows];
+        for (int i = 0; i < l.numRows; i++) {
+            this->seatsPerRow[i] = l.seatsPerRow[i];
+        }
+        numZones = l.numZones;
+        this->zones = new string[l.numZones];
+        for (int i = 0; i < l.numZones; i++) {
+            this->zones[i] = l.zones[i];
+        }
+
+       
+    }
+    return *this;
+}
+
+std::istream& operator>>(std::istream& is, EventLocation& eventLocation) {
+    cout << "nomax: ";
+    is >> eventLocation.maxSeats >> eventLocation.numRows;
+    delete[] eventLocation.zones;
+    delete[] eventLocation.seatsPerRow;
+    cout << "no of zones";
+    is >> eventLocation.numZones;
+    eventLocation.zones = new string[eventLocation.numZones];
+    cout << "zones: ";
+ 
+    for (int i = 0; i < eventLocation.numZones; i++) {
+        is >> eventLocation.zones[i];
+    } 
+    cout << "no of rows";
+    is >> eventLocation.numRows;
+    eventLocation.seatsPerRow = new int[eventLocation.numRows];
+    cout << "seats: ";
+    for (int i = 0; i < eventLocation.numRows; i++) {
+        is >> eventLocation.seatsPerRow[i];
+    }
+
+    return is;
+}
+
+ std::ostream& operator<<(std::ostream& os, const EventLocation& eventLocation) {
+    os << "Max Seats: " << eventLocation.maxSeats << "endl";
+    os << "Num Rows: " << eventLocation.numRows << "endl";
+    os << "Num zones: " << eventLocation.numZones << "endl";
+    os << "Zones: "  << "endl";
+    for (int i = 0; i < eventLocation.numZones; i++) {
+        os << eventLocation.zones[i] << " ";
+    }
+    
+    os << "Seats Per Row: ";
+    for (int i = 0; i < eventLocation.numRows; i++) {
+        os << eventLocation.seatsPerRow[i] << " ";
+    }
+    os << "endl";
+
+    return os;
+}
+
+int EventLocation::getMaxSeats() const {
+    return maxSeats;
+}
+
+void EventLocation::setMaxSeats(int maxSeats) {
+    this->maxSeats = maxSeats;
+}
+
+int EventLocation::getNumRows() const {
+    return numRows;
+}
+
+
+void EventLocation::setZones(const std::string* newZones, int newNumZones) {
+    delete[] zones;
+    numZones = newNumZones;
+    if (newZones) {
+        zones = new std::string[numZones];
+        for (int i = 0; i < numZones; i++) {
+            zones[i] = newZones[i];
+        }
+    }
+    else {
+        zones = nullptr;
+    }
+}
+
+
+int*  EventLocation:: getSeatsPerRow()const  {
+    return seatsPerRow;
+}
+
+void  EventLocation::setSeatsPerRow(int* newSeatsPerRow,int num) {
+    delete[] seatsPerRow;
+    numRows = num;
+    seatsPerRow = new int[numRows];
+    for (int i = 0; i < numRows; i++) {
+        seatsPerRow[i] = newSeatsPerRow[i];
+    }
+}
+
+int& EventLocation::operator[](int rowIndex) {
+
+    if (rowIndex < 0 && rowIndex >= numRows) {
+        throw std::out_of_range("Invalid row index");
+    } 
+
+    return seatsPerRow[rowIndex];
+}
+
+
+int EventLocation::operator()() const {
+    int totalSeats = 0;
+    for (int i = 0; i < numRows; i++) {
+        totalSeats += seatsPerRow[i];
+    }
+    return totalSeats;
+}
