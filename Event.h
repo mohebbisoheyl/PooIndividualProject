1d8c060501f272efa7973ec88f4b1e0df2c88ee8
mohebbisoheyl <74722171+mohebbisoheyl@users.noreply.github.com>
2023-11-26 17:38:52 +0200

    Update Event.h

--- Event.h
+++ Event.h
@@ -5,34 +5,38 @@
 #include <iostream>
 
 class Event {
-
 private:
-unsignet const int id;
     char* date;
     char* time;
     char* name;
-static int counter;
 
 public:
     
     Event();
     Event(const char* date, const char* time, const char* name);
-
-   
+    Event(const Event& e);
     ~Event();
 
-  
+    
     Event& operator=(const Event& other);
 
+   
+    const char* getDate() const;
+    void setDate(const char* date);
 
-    friend std::istream& operator>>(std::istream& in, Event& event);
+    const char* getTime() const;
+    void setTime(const char* time);
 
-    friend std::ostream& operator<<(std::ostream& out, const Event& event);
-
-
+    const char* getName() const;
+    void setName(const char* name);
 
    
-};
-int Event::counter=1;
-#endif
+    bool operator==(const Event& other) const;
+    bool operator!=(const Event& other) const;
 
+   
+    friend std::istream& operator>>(std::istream& in, Event& event);
+    friend std::ostream& operator<<(std::ostream& out, const Event& event);
+};
+
+#endif // EVENT_H
